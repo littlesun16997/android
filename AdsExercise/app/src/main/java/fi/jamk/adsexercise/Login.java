6b1ba03fb5f67e98f7fc9eed62690fdeb363d856
L8445 <L8445@student.jamk.fi>
2018-06-12 23:27:20 +0300

    AdsExercise is ready

---
+++ AdsExercise/app/src/main/java/fi/jamk/adsexercise/Login.java
@@ -0,0 +1,113 @@
+package fi.jamk.adsexercise;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Login extends AppCompatActivity {
+    TextView register;
+    EditText username, password;
+    Button loginButton;
+    String user, pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        MobileAds.initialize(this, "ca-app-pub-5721987121244895~4581383274");
+
+        // Load an ad into the AdMob banner view.
+        AdView adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template").build();
+        adView.loadAd(adRequest);
+
+        register = (TextView)findViewById(R.id.register);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        loginButton = (Button)findViewById(R.id.loginButton);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = username.getText().toString();
+                pass = password.getText().toString();
+
+                if(user.equals("")){
+                    username.setError("can't be blank");
+                }
+                else if(pass.equals("")){
+                    password.setError("can't be blank");
+                }
+                else{
+                    String url = "https://adsexercise.firebaseio.com/users.json";
+
+                    StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+                        @Override
+                        public void onResponse(String s) {
+                            if(s.equals("null")){
+                                Toast.makeText(Login.this, "user not found", Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                try {
+                                    JSONObject obj = new JSONObject(s);
+
+                                    if(!obj.has(user)){
+                                        Toast.makeText(Login.this, "user not found", Toast.LENGTH_LONG).show();
+                                    }
+                                    else if(obj.getJSONObject(user).getString("password").equals(pass)){
+                                        UserDetails.username = user;
+                                        UserDetails.password = pass;
+                                        startActivity(new Intent(Login.this, Users.class));
+                                    }
+                                    else {
+                                        Toast.makeText(Login.this, "incorrect password", Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }
+                    },new Response.ErrorListener(){
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            Log.e("error", "" + volleyError);
+                        }
+                    });
+
+                    RequestQueue rQueue = Volley.newRequestQueue(Login.this);
+                    rQueue.add(request);
+                }
+
+            }
+        });
+    }
+}
